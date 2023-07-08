983884a09a28c4f3cca90b33c43b56e697ddba39
Hardik Narang <126074137+Hardik-2311@users.noreply.github.com>
2023-07-08 19:49:20 +0530

    tf left done

--- src/components/TopFold/index.js
+++ src/components/TopFold/index.js
@@ -11,11 +11,28 @@ const Topfold = () => {
                 </div>
                 <div className='tf-description'>cbeWPGSUDCEWuawuEBJCKWalsxnawpejkdbvc cpwebk;jacskdbjzc cepiwpiuq[fuwecjweouc[h2J{/* U43 */}</div>
                 <div className='tf-left-buttons'>
-                    <Button btnType="PRIMARY" btnText="Explore"/>
-                    <Button btnType="SECONDARY" btnText="Create"/>
-                </div> 
+                    <Button btnType="PRIMARY" btnText="Explore" />
+                    <Button btnType="SECONDARY" btnText="Create" />
+                </div>
+                <div className='tf-left-info'>
+                    <div className='tf-info-element absolute-center'>
+                        <div className='tf-count'>300K+</div>
+                        <div className='tf-label'>Collections</div>
+                    </div>
+                    <div className='tf-info-element absolute-center'>
+                        <div className='tf-count'>40K+</div>
+                        <div className='tf-label'>Artists</div>
+                    </div>
+                    <div className='tf-info-element absolute-center'>
+                        <div className='tf-count'>563K+</div>
+                        <div className='tf-label'>Communities</div>
+                    </div>
+                </div>
+
             </div>
-            <div className='tf-right'></div>
+            <div className='tf-right'>
+                
+            </div>
         </div>
     )
 }

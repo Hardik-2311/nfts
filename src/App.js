8ad7c0a6cc100dc1d40fb101736ae64c108e0043
Hardik Narang <126074137+Hardik-2311@users.noreply.github.com>
2023-08-02 06:09:09 +0530

    chnages reviewed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import React from 'react'
 import "./App.css"
 import Header from './components/Header';
-import TopFold from './components/TopFold';
-import Brands from './components/Brands';
-import Trending from './components/Trending';
-import Info from './components/Info';
-import Footer from './components/Footer';
+import TopFold from './components/TopFold';;
 
 const App = () => {
   return (
     <div className='max-width'>
       <Header />
       <TopFold />
-      <Brands />
-      <Trending />
-      <Info />
-      <Footer />
-
     </div>
   )
 }
--- src/components/TopFold/index.js
+++ src/components/TopFold/index.js
@@ -6,8 +6,8 @@ const Topfold = () => {
     return (
         <div className='TopFold absolute-center'>
             <div className='tf-left'>
-                <div className='tf-Heading'>
-                    Discover,Collect, & Sell <span className='Header-gradient'>Extraordinary</span>NFT's
+                <div className='tf-Heading'> 
+                    Discover,Collect, &Sell <span className='Header-gradient'>Extraordinary</span> NFT's
                 </div>
                 <div className='tf-description'>cbeWPGSUDCEWuawuEBJCKWalsxnawpejkdbvc cpwebk;jacskdbjzc cepiwpiuq[fuwecjweouc[h2J{/* U43 */}</div>
                 <div className='tf-left-buttons'>
